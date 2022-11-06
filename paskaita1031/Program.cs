e6a94a21444e15eb8e6a394541dbc47a000af825
Kasparas <kasparas.mociunas@gmail.com>
2022-11-06 22:47:43 +0200

    trecias

--- paskaita1031/Program.cs
+++ paskaita1031/Program.cs
@@ -11,8 +11,8 @@ namespace ReadATextFile
     {
 
         // Default folder    
-        static readonly string rootFolder = "/Users/admin/Desktop/vigi .NET/paskaita1031/products";
-        static readonly string textFileSweets = "/Users/admin/Desktop/vigi .NET/paskaita1031/products/Sweets - Sheet1.csv";
+        static readonly string rootFolder =  "/Users/admin/Desktop/vigi .NET/paskaita1031/products";    
+        static readonly string textFileSweets =  "/Users/admin/Desktop/vigi .NET/paskaita1031/products/Sweets - Sheet1.csv";
         static readonly string textFileMeat = "/Users/admin/Desktop/vigi .NET/paskaita1031/products/Meat - Sheet1.csv";
         static readonly string textFileGroceries = "/Users/admin/Desktop/vigi .NET/paskaita1031/products/Groceries - Sheet1.csv";
         static readonly string textFileLiquids = "/Users/admin/Desktop/vigi .NET/paskaita1031/products/Liquids - Sheet1.csv";  //sudeti nuorodas i kitu kategoriju produktus
@@ -22,17 +22,68 @@ namespace ReadATextFile
 
             Shop shop = new Shop();
 
+             List<Product> fullProductList = new List<Product>();
+             List<Product> myShoppingCart = new List<Product>();
+
+           
+
+           
+
+            shop.fullShopProductList = fullProductList;
+            shop.shoppingCart = myShoppingCart;
+
+            if (File.Exists(textFileSweets))
+            {
+                shop.ReadFileAddProduct(textFileSweets, "Sweets", fullProductList);
+            }
+            if (File.Exists(textFileMeat))
+            {
+                shop.ReadFileAddProduct(textFileMeat, "Meat", fullProductList);
+            }
+            if (File.Exists(textFileGroceries))
+            {
+                shop.ReadFileAddProduct(textFileGroceries, "Groceries", fullProductList);
+            }
+            if (File.Exists(textFileLiquids))
+            {
+                shop.ReadFileAddProduct(textFileLiquids, "Liquids", fullProductList);
+            }
 
 
 
 
 
+            Console.WriteLine("enter your budget: ");
+            shop.credit = Convert.ToDouble(Console.ReadLine());
+
+            shop.CheckBudget(shop.credit);
+            shop.ShowMainMenu();
+
+
+
+
+
+
+
+
+
+
+
+            Console.WriteLine();
+            Console.ReadKey();
 
         }
 
 
+       
+
+
+          
+        }
+    
+       
     }
-}
+
 
 
 
